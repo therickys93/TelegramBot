e1fb8db1ae0aaba36a30a4ef2602082f0cef76ae
Riccardo Crippa <therickys93@gmail.com>
2016-09-10 14:24:18 +0200

    tolti i commenti

--- Tests/TelegramBotTests/TelegramBotTests.swift
+++ Tests/TelegramBotTests/TelegramBotTests.swift
@@ -3,8 +3,6 @@ import XCTest
 
 class TelegramBotTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertEqual(TelegramBot().text, "Hello, World!")
     }
 
